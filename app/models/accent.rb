4ac2eff7621bd138bd0bfb1e51dbb2db375c3fcb
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-01-14 19:04:30 +0900

    バリデーションの追記

--- app/models/accent.rb
+++ app/models/accent.rb
@@ -7,9 +7,13 @@ class Accent < ApplicationRecord
   belong_to :beat_count
 
   with_options presence: true do
-    validates :word
-    validates :word_kana
-    
+    validates :info, length: { maximum: 400 } 
+
+    with_options length: { maximum: 20 } do
+      validates :word
+      validates :word_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
+    end
+
     with_options numericality: { other_than: 1, messages: "can't be blank"} do
       validates :part_of_speech_id
       validates :accent_pattern_id
