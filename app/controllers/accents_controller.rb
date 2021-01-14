edd0092232a5e6240f835d85550d34443f6682e1
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-01-15 01:52:29 +0900

    accentsテーブルへの保存

--- app/controllers/accents_controller.rb
+++ app/controllers/accents_controller.rb
@@ -3,5 +3,25 @@ class AccentsController < ApplicationController
   end
 
   def new
+    @accent = Accent.new
+  end
+
+  def create
+    @accent = Accent.new(accent_params)
+    if @accent.valid?
+      @accent.save
+      redirect_to @accent
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    @accent = Accent.find(params[:id])
+  end
+
+  private
+  def accent_params
+    params.require(:accent).permit(:word, :word_kana, :part_of_speech_id, :accent_pattern_id, :beat_count_id, :info).merge(user_id: current_user.id)
   end
 end
--- app/models/accent.rb
+++ app/models/accent.rb
@@ -2,9 +2,9 @@ class Accent < ApplicationRecord
   belongs_to :user
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belong_to :part_of_speech
-  belong_to :accent_pattern
-  belong_to :beat_count
+  belongs_to :part_of_speech
+  belongs_to :accent_pattern
+  belongs_to :beat_count
 
   with_options presence: true do
     validates :info, length: { maximum: 400 } 
@@ -14,7 +14,7 @@ class Accent < ApplicationRecord
       validates :word_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters." }
     end
 
-    with_options numericality: { other_than: 1, messages: "can't be blank"} do
+    with_options numericality: { other_than: 1, message: "can't be blank"} do
       validates :part_of_speech_id
       validates :accent_pattern_id
     end
