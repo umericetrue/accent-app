769cf0d056cd719257303f433511805aa4ee6dc4
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-01-19 15:22:19 +0900

    モデル単体テストの準備

---
+++ spec/factories/accents.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :accent do
+    word {Faker::Lorem.characters(number: 20)}
+    word_kana {"テスト"}
+    part_of_speech_id { 2 }
+    accent_pattern_id { 2 }
+    beat_count_id { 2 }
+    info {Faker::Lorem.sentence}
+    association :user
+  end
+end
