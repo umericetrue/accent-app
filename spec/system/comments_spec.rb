783d770f0fd876ed9b07d5fa10b845ab5b88be69
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-01-21 13:54:12 +0900

    コメント機能の結合テスト

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :system do
+RSpec.describe "コメント投稿機能", type: :system do
   before do
-    driven_by(:rack_test)
+    @user = FactoryBot.create(:user)
+    @accent = FactoryBot.create(:accent)
+    @comment = FactoryBot.build(:comment)
   end
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+  it 'ログインしたユーザーはアクセント詳細ページでコメント投稿できる' do
+    sign_in(@user)
+    visit accent_path(@accent)
+    fill_in 'comment[comment]', with: @comment
+    expect{
+      click_on ('コメントする')
+    }.to change { Comment.count }.by(1)
+    expect(current_path).to eq accent_path(@accent)
+    expect(page).to have_content @comment
+  end
 end
