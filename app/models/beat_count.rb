bf68cc90dec0dd0983c3cc70529947b7915929d7
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-02-23 12:47:01 +0900

    beat_countのセレクトボックス表示修正

--- app/models/beat_count.rb
+++ app/models/beat_count.rb
@@ -1,24 +1,24 @@
 class BeatCount < ActiveHash::Base
   self.data = [
     { id: 1, name: '---' },
-    { id: 2, name: '2' },
-    { id: 3, name: '3' },
-    { id: 4, name: '4' },
-    { id: 5, name: '5' },
-    { id: 6, name: '6' },
-    { id: 7, name: '7' },
-    { id: 8, name: '8' },
-    { id: 9, name: '9' },
-    { id: 10, name: '10' },
-    { id: 11, name: '11' },
-    { id: 12, name: '12' },
-    { id: 13, name: '13' },
-    { id: 14, name: '14' },
-    { id: 15, name: '15' },
-    { id: 16, name: '16' },
-    { id: 17, name: '17' },
-    { id: 18, name: '18' },
-    { id: 19, name: '19' }
+    { id: 2, name: '2拍目' },
+    { id: 3, name: '3拍目' },
+    { id: 4, name: '4拍目' },
+    { id: 5, name: '5拍目' },
+    { id: 6, name: '6拍目' },
+    { id: 7, name: '7拍目' },
+    { id: 8, name: '8拍目' },
+    { id: 9, name: '9拍目' },
+    { id: 10, name: '10拍目' },
+    { id: 11, name: '11拍目' },
+    { id: 12, name: '12拍目' },
+    { id: 13, name: '13拍目' },
+    { id: 14, name: '14拍目' },
+    { id: 15, name: '15拍目' },
+    { id: 16, name: '16拍目' },
+    { id: 17, name: '17拍目' },
+    { id: 18, name: '18拍目' },
+    { id: 19, name: '19拍目' }
   ]
 
   include ActiveHash::Associations
