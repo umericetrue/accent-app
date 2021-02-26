8dafe93cc32fb260220e5df7946ec75f9d46e3f5
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-02-26 16:06:15 +0900

    頭高型投票機能の投票できる時の結合テスト

---
+++ spec/system/atamadakas_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+def basic_pass(path)
+  username = ENV["BASIC_AUTH_USER"]
+  password = ENV["BASIC_AUTH_PASSWORD"]
+  visit "http://#{username}:#{password}@#{Capybara.current_session.server.host}:#{Capybara.current_session.server.port}#{path}"
+end
+
+RSpec.describe "頭高型投票機能(いいね機能)", type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @accent = FactoryBot.create(:accent)
+  end
+
+  context '投票できるとき' do
+    it 'ログインしたユーザーはアクセント詳細ページで頭高型の投票機能を使うことができる' do
+      basic_pass root_path
+      sign_in(@user)
+      visit accent_path(@accent)
+      expect {
+        click_link '頭高型'
+      }.to change { Atamadaka.count }.by(1)
+      expect(current_path).to eq accent_path(@accent)
+    end
+  end
+end
