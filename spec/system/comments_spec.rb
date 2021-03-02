14cb528a4dc98b60c916d834c1c4e4362c68bc69
umericetrue <76139604+umericetrue@users.noreply.github.com>
2021-03-02 11:44:47 +0900

    コメント投稿できない時のコメント結合テスト

--- spec/system/comments_spec.rb
+++ spec/system/comments_spec.rb
@@ -6,16 +6,26 @@ RSpec.describe 'コメント投稿機能', type: :system do
     @accent = FactoryBot.create(:accent)
     @comment = FactoryBot.build(:comment)
   end
-
-  it 'ログインしたユーザーはアクセント詳細ページでコメント投稿できる' do
-    basic_pass root_path
-    sign_in(@user)
-    visit accent_path(@accent)
-    fill_in 'comment[comment]', with: @comment
-    expect  do
-      click_on('コメントする')
-    end.to change { Comment.count }.by(1)
-    expect(current_path).to eq accent_path(@accent)
-    expect(page).to have_content @comment
+  context 'コメント投稿できる時' do
+    it 'ログインしたユーザーはアクセント詳細ページでコメント投稿できる' do
+      basic_pass root_path
+      sign_in(@user)
+      visit accent_path(@accent)
+      fill_in 'comment[comment]', with: @comment
+      expect  do
+        click_on('コメントする')
+      end.to change { Comment.count }.by(1)
+      expect(current_path).to eq accent_path(@accent)
+      expect(page).to have_content @comment
+    end
+  end
+  
+  context 'コメント投稿できない時' do
+    it 'ログインしていないユーザーはコメント投稿できない' do
+      basic_pass root_path
+      visit accent_path(@accent)
+      expect(page).to have_no_selector 'form'
+      expect(page).to have_content('コメントの投稿には新規登録/ログインが必要です')
+    end
   end
 end
